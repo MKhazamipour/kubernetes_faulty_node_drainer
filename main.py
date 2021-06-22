513e98b8e4487190425875489f2d495758784ca1
Morteza Khazamipour <mormoroth@gmail.com>
2021-06-23 00:59:22 +0430

    removed redis

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import subprocess
 import os
 
-#Check the status of servers
-  #if all servers are okay flush redis
-    # uncordon drained node
-  #if any server is faulty add it to redis
-    # drain faulty servers
 kubeconfig_file = os.getenv('kube_config_path')
 
-#r = redis.Redis(host='localhost', port=6379)
 
 def get_server_list():
     servers = []
@@ -31,65 +25,3 @@ def cordon_faulty_servers(bad_server):
 
 
 cordon_faulty_servers(get_server_list())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" def add_to_redis(sv_ip):
-    if len(sv_ip) == 0:
-        print("flushing redis")
-        r.flushall()
-    else:
-        for i in range(len(sv_ip)):
-            try:
-                r.set('server_number'+str(i), servers[i])
-                i += 1
-            except redis.ConnectionError:
-                print("Unable to connect to redis")
-                sys.exit(1)
-
-
-
-for srv in r.get():
-    print(srv)
-        
-#k = subprocess.run(
-#    ['kubectl', '--kubeconfig', kubeconfig_file, 'get', 'pods','--all-namespaces'],
-#    stdout=subprocess.PIPE,
-#    stderr=subprocess.PIPE
-#)
-#
-#print(k.stdout.decode())
-#print(k.stderr.decode())
-
-to_redis = get_server_list()
-add_to_redis(to_redis)
-
-
- """
-
-
-
-
-
-
