3431c6c3c2e01365f1a714ff1a1baa6b8482a30f
Morteza Khazamipour <mormoroth@gmail.com>
2021-06-23 10:41:21 +0430

    handle error if kubeconfig path is not provided

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 from server_list import get_server_list
 
@@ -9,14 +10,18 @@ faulty_servers = get_server_list()
 
 def cordon_faulty_servers(sv):
     for i in range(len(sv)):
-        node_drain = subprocess.run(
-                     ['kubectl', '--kubeconfig', kubeconfig_file, 'drain', sv[i],'--force', '--grace-period=0','--ignore-daemonsets','--delete-local-data'],
-                     stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE
-                                    )
-        print(node_drain.stdout.decode())
-        print(node_drain.stderr.decode())
-        i +=1
+        try:
+            node_drain = subprocess.run(
+                        ['kubectl', '--kubeconfig', kubeconfig_file, 'drain', sv[i],'--force', '--grace-period=0','--ignore-daemonsets','--delete-local-data'],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                                        )
+            print(node_drain.stdout.decode())
+            print(node_drain.stderr.decode())
+            i +=1
+        except TypeError:
+            print("Please provide 'kube_config_path' path as environment variable")
+            sys.exit(1)
 
 
 cordon_faulty_servers(faulty_servers)
